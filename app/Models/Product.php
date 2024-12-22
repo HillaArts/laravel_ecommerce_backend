8e680a04d6b0b136d07b15a3dde4a7c00a13c740
HillaArts <hassenyehillary@gmail.com>
2024-12-23 00:38:41 +0300

    Add Product model with attributes, relationships, and price formatting methods

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,9 +2,56 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Product Model
+ *
+ * Represents a product that can be added to an order. Tracks attributes
+ * such as `name` and `price`. Supports relationships with order items.
+ */
 class Product extends Model
 {
-    //
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['name', 'price'];
+
+    /**
+     * Get the order items associated with the product.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    /**
+     * Scope a query to only include products within a specific price range.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param float $min
+     * @param float $max
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePriceBetween($query, float $min, float $max)
+    {
+        return $query->whereBetween('price', [$min, $max]);
+    }
+
+    /**
+     * Get the formatted price of the product.
+     *
+     * @return string
+     */
+    public function getFormattedPriceAttribute()
+    {
+        return number_format($this->price, 2);
+    }
 }
