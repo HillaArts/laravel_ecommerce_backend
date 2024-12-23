6332c93295199fd3e4c20b569ef53b0ab684f7e7
HillaArts <hassenyehillary@gmail.com>
2024-12-24 00:22:43 +0300

    Refactor API routes to remove authentication middleware for cart and order endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,79 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\OrderController;
-use Illuminate\Support\Facades\Route;
 
-/**
- * Group routes that require authentication middleware.
- *
- * This ensures that all the routes within this group require the user to be authenticated
- * via Sanctum before accessing the associated controller methods.
- */
-Route::middleware('auth:sanctum')->group(function () {
+// Test routes without authentication
+Route::prefix('cart')->group(function () {
+    Route::post('/', [CartController::class, 'addToCart'])->name('cart.add');
+    Route::delete('/{productId}', [CartController::class, 'removeFromCart'])->name('cart.remove');
+    Route::get('/', [CartController::class, 'viewCart'])->name('cart.view');
+});
 
-    /**
-     * Cart Routes
-     *
-     * These routes handle operations related to the user's cart,
-     * such as adding, removing, and viewing items in the cart.
-     */
-    Route::prefix('cart')->group(function () {
-
-        /**
-         * Add a product to the cart.
-         *
-         * This route allows an authenticated user to add a product to their shopping cart.
-         *
-         * @param  int  $productId  The ID of the product to be added.
-         * @return \Illuminate\Http\Response
-         */
-        Route::post('/', [CartController::class, 'addToCart'])->name('cart.add');
-
-        /**
-         * Remove a product from the cart.
-         *
-         * This route allows an authenticated user to remove a product from their shopping cart.
-         *
-         * @param  int  $productId  The ID of the product to be removed.
-         * @return \Illuminate\Http\Response
-         */
-        Route::delete('/{productId}', [CartController::class, 'removeFromCart'])->name('cart.remove');
-
-        /**
-         * View the current cart.
-         *
-         * This route allows an authenticated user to view all the products currently in their cart.
-         *
-         * @return \Illuminate\Http\Response
-         */
-        Route::get('/', [CartController::class, 'viewCart'])->name('cart.view');
-    });
-
-    /**
-     * Order Routes
-     *
-     * These routes manage the user's order-related operations, such as placing and viewing orders.
-     */
-    Route::prefix('orders')->group(function () {
-
-        /**
-         * Place an order.
-         *
-         * This route allows an authenticated user to place an order based on the items in their cart.
-         *
-         * @return \Illuminate\Http\Response
-         */
-        Route::post('/', [OrderController::class, 'placeOrder'])->name('orders.place');
-
-        /**
-         * View user's orders.
-         *
-         * This route allows an authenticated user to view all of their past orders.
-         *
-         * @return \Illuminate\Http\Response
-         */
-        Route::get('/', [OrderController::class, 'viewOrders'])->name('orders.view');
-    });
-
+Route::prefix('orders')->group(function () {
+    Route::post('/', [OrderController::class, 'placeOrder'])->name('orders.place');
+    Route::get('/', [OrderController::class, 'viewOrders'])->name('orders.view');
 });
